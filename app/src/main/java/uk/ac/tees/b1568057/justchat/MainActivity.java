917acc9b300c901db81dd4dea5159925994cd883
alvinpeter9 <alvinpeter9@gmail.com>
2022-04-16 15:32:13 +0100

    third commit - user profile completed

--- app/src/main/java/uk/ac/tees/b1568057/justchat/MainActivity.java
+++ app/src/main/java/uk/ac/tees/b1568057/justchat/MainActivity.java
@@ -23,9 +23,131 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
+    TabLayout tabLayout;
+    TabItem mchat,mcall,mstatus;
+    ViewPager viewPager;
+    PagerAdapter pagerAdapter;
+    androidx.appcompat.widget.Toolbar mtoolbar;
+
+    FirebaseAuth firebaseAuth;
+
+
+    FirebaseFirestore firebaseFirestore;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-}
+
+        tabLayout=findViewById(R.id.include);
+        mchat=findViewById(R.id.chat);
+        mcall=findViewById(R.id.calls);
+        mstatus=findViewById(R.id.status);
+        viewPager=findViewById(R.id.fragmentcontainer);
+
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        mtoolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(mtoolbar);
+
+
+        Drawable drawable= ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_baseline_more_vert_24);
+        mtoolbar.setOverflowIcon(drawable);
+
+
+        pagerAdapter=new uk.ac.tees.b1568057.justchat.Adapter.PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+                if(tab.getPosition()==0 || tab.getPosition()==1|| tab.getPosition()==2)
+                {
+                    pagerAdapter.notifyDataSetChanged();
+                }
+
+
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.profile:
+                Intent intent = new Intent(getApplicationContext(),ProfileActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.settings:
+                Toast.makeText(getApplicationContext(),"Settings is clicked",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+
+
+        return  true;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+
+        return true;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        DocumentReference documentReference=firebaseFirestore.collection("Users").document(firebaseAuth.getUid());
+        documentReference.update("status","Offline").addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getApplicationContext(),"Now User is Offline",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        DocumentReference documentReference=firebaseFirestore.collection("Users").document(firebaseAuth.getUid());
+        documentReference.update("status","Online").addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getApplicationContext(),"Now User is Online",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
 }
